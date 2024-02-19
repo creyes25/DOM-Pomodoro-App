6a7e9aa321c47ff7073396d6cc6be43401b03d0a
creyes25 <carolina.reyes2022@gmail.com>
2024-02-19 02:10:18 -0600

    add functinality to change the status of the timer and reset after all cycles of pomodoro is complete

--- script.js
+++ script.js
@@ -9,50 +9,89 @@ const resetBtn = document.querySelector('.reset-btn')
 // POMODORO
 const defaultTimerSettings = {
   timer : 1,
-  shortBreak : 5,
-  longBreak : 60,
-  totalCycles: 4,
+  shortBreak : 2,
+  longBreak : 3,
+  totalCycles: 2,
   currentStatus : 'pomodoro'
 }
 
 let timerInSeconds = defaultTimerSettings.timer * 60
 let shortBreakInSec = defaultTimerSettings.shortBreak * 60
-let longBreakInSec = defaultTimerSettings.longBreakBreak * 60
+let longBreakInSec = defaultTimerSettings.longBreak * 60
 let totalCycles = defaultTimerSettings.totalCycles
+let currentStatus = defaultTimerSettings.currentStatus
 let initialCycle = 1
 let minutes, seconds
 let isPaused, isReset = false
+let timer = currentTimer()
 
 
-countdownDisplay(timerInSeconds)
+
+
+countdownDisplay(timer)
 currentCycle.innerHTML = initialCycle
 cyclesTotal.innerHTML = totalCycles
 
-function startTimer() {
-  const interval = setInterval(() => {
-    
-    if(isPaused) {
-      clearInterval(interval)
-    }
-    
-    if(isReset) {
-      clearInterval(interval)
-      resetTimer()
-    }
-
-    countdownDisplay(timerInSeconds)
-    timerInSeconds--
-    
-    if (timerInSeconds < 0) {
-      timerInSeconds = 0
-      clearInterval(interval)
-      disableStartBtn(false)
-    }
-    
-    
-    
-  }, 1000)
+function currentTimer() {
+  if (currentStatus === 'pomodoro') {
+    return timerInSeconds
+  }else if (currentStatus === 'short') {
+    return shortBreakInSec
+  }else if (currentStatus === 'long') {
+    return longBreakInSec
+  }
 }
+
+function startTimer() {
+
+  if (initialCycle > totalCycles) {
+    initialCycle = 1
+    console.log('Youre done with all your cycles')
+    alert('All Cycles are done!')
+  } else {
+    currentCycle.innerHTML = initialCycle
+    const interval = setInterval(() => {
+      if(isPaused) {
+        clearInterval(interval)
+      }
+      
+      if(isReset) {
+        clearInterval(interval)
+        resetTimer()
+      }
+  
+      countdownDisplay(timer)
+      timer--
+      
+      
+      if (timer < 0) {
+        isPaused = true
+        clearInterval(interval)
+        disableStartBtn(false)
+
+        // can create a new function changeStatus
+        if(currentStatus === 'pomodoro') {
+          currentStatus = 'short'
+          timer = currentTimer()
+          startTimer()
+        } else if (currentStatus === 'short') {
+          currentStatus = 'long'
+          timer = currentTimer()
+          startTimer()
+        } else if (currentStatus === 'long') {
+          currentStatus = 'pomodoro'
+          timer = currentTimer()
+          initialCycle++
+          startTimer()
+        }
+      }
+    }, 100)
+
+  }
+
+}
+
+
 
 function resetTimer() {
   timerInSeconds = defaultTimerSettings.timer * 60
